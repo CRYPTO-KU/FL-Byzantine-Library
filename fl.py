c925d8adfe6402eda5eba347d8a1c8cd017dfa7e
Kerem Özfatura <keremozfatura@me.com>
2025-05-30 16:58:41 +0300

    FL class

--- fl.py
+++ fl.py
@@ -80,8 +80,6 @@ class FL(object):
         #Client selection
         selected_benigns = [client for client in all_clients if client.id not in self.traitors]
         selected_malicious = [client for client in all_clients if client.id in self.traitors]
-        #print(len(selected_benigns),len(selected_malicious))
-        #training
         [client.train_(net=self.net_ps) for client in selected_benigns]
         benign_grads = [cl.get_grad() for cl in selected_benigns]
         if len(selected_malicious) > 0:
@@ -115,6 +113,10 @@ class FL(object):
         return all_grads
 
     def Byzantine_grad_preds(self):
+        '''
+        Function for non-omniscient Byzantine Attackers.
+        Attackers use their own data to generate Psuedo-Grads and momentums
+        '''
         pred_per_byzantine = int((1-self.args.traitor) / self.args.traitor)
         if self.psuedo_momentums is None:
             self.psuedo_momentums = [torch.tensor(torch.zeros(count_parameters(self.net_ps)),device=self.device) for cl in self.benign_clients]
@@ -140,72 +142,3 @@ class FL(object):
         return self.aggr.__call__(all_grads)
 
 
-    def std_analysis(self,all_grads):
-        stacked_gradients = torch.stack(all_grads, 1)
-        mu = torch.mean(stacked_gradients, 1).to(self.device)
-        std = torch.std(stacked_gradients, 1).to(self.device)
-        stacked_gradients = stacked_gradients.permute(1,0)
-        distance_to_mean = torch.abs((stacked_gradients - mu) / std)
-        std_vec = distance_to_mean
-        outliers_val,outliers_inds = torch.topk(std_vec,dim=0, k=5, largest=True)
-        non_outlier_val, _ = torch.topk(std_vec,dim=0, k=20, largest=False)
-        all_median,_ = torch.median(std_vec, dim=0)
-        max_non_outlier,_ = torch.max(non_outlier_val,0)
-        average_outlier_distance = torch.mean(outliers_val, 0)
-        outlier_median, _ = torch.median(outliers_val, dim=0)
-        cheby_density = (std_vec > 1.41).float().mean(0)
-        cheby_density2 = (non_outlier_val > 1.41).float().sum(0)
-        locs,crits = get_layer_dims2(self.net_ps)
-        layer_names = get_layer_names(self.net_ps)
-        if self.layer_inspection is None:
-            self.create_layer_dict()
-        for inds, is_crit,name in zip(locs,crits,layer_names):
-            if not is_crit: # weights only
-                self.layer_inspection['median'][name].append(all_median[inds].mean().detach().cpu().numpy())
-                self.layer_inspection['avg_outlier_dist'][name].append(average_outlier_distance[inds].mean().detach().cpu().numpy())
-                self.layer_inspection['outlier_median'][name].append(outlier_median[inds].mean().detach().cpu().numpy())
-                self.layer_inspection['density'][name].append(cheby_density[inds].mean().detach().cpu().numpy())
-                self.layer_inspection['density2'][name].append(cheby_density2[inds].mean().detach().cpu().numpy())
-
-
-    def layer_std_analysis(self,epoch):
-        layers = get_layer_names(self.net_ps)
-        locs, crits = get_layer_dims2(self.net_ps)
-        selected_layers = [name for name,crit in zip(layers,crits) if not crit]
-
-        x = np.arange(len(selected_layers))  # the label locations
-        width = 0.2  # the width of the bars
-        multiplier = 0
-
-        fig, ax = plt.subplots(layout='constrained')
-
-        layer_dic = self.layer_inspection
-        final_dic = {key:[] for key in self.layer_dic.keys()}
-        for key in final_dic.keys():
-            for layer in layer_dic[key]:
-                final_dic[key].append(np.mean(layer_dic[key][layer]))
-
-        for attribute, measurement in final_dic.items():
-            offset = width * multiplier
-            rects = ax.bar(x + offset, np.round(measurement,2), width, label=attribute)
-            ax.bar_label(rects, padding=3)
-            multiplier += 1
-
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        ax.set_ylabel('STD multipler')
-        ax.set_xlabel('Layers')
-        ax.set_xticks(x + width, selected_layers)
-        ax.legend(loc='upper left')
-        fig = plt.gcf()
-        fig.set_size_inches(26, 10)
-        plt.savefig('STDFigs/layer_std_analysis{}-{}-{}.png'.format(self.args.aggr,self.args.dataset_dist,epoch))
-        #plt.show()
-        return
-
-    def create_layer_dict(self):
-        locs,crits = get_layer_dims2(self.net_ps)
-        layer_names = get_layer_names(self.net_ps)
-
-        layer_dic = {name:[] for name,crit in zip(layer_names,crits) if not crit}
-        final_dic = {key:deepcopy(layer_dic) for key in self.layer_dic.keys()}
-        self.layer_inspection = final_dic
